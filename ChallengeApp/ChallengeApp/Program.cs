530392d446052fb4a2c54cf8c7b85ada78a735de
testwriter1 <mj.mjab66@gmail.com>
2023-05-02 14:26:41 +0200

    Zadanie dzień 3

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var surname = "Belek";
+var sex = false;
+var age = 16;
+
+if (sex == false)
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else
+    {
+        Console.WriteLine(name + ", lat " + age);
+    }
+}
+else
+{
+
+
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+    else
+    {
+        Console.WriteLine(name + ", lat " + age);
+    }
+
+
+}
